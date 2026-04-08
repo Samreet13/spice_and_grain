2779c2ae02fe36e6bb0a33f3d9bd81d33823ad31
Samreet13 <skaur228@rrc.ca>
2026-04-08 10:27:52 -0500

    Added admin control to update order status using ActiveAdmin

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,18 +1,36 @@
 ActiveAdmin.register Order do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :total, :province, :subtotal, :tax, :customer_id, :gst, :pst, :hst, :province_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:total, :province, :subtotal, :tax, :customer_id, :gst, :pst, :hst, :province_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :status
+
+  index do
+    selectable_column
+    id_column
+    column :customer
+    column :total
+    column :status
+    column :created_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :customer
+      row :subtotal
+      row :gst
+      row :pst
+      row :hst
+      row :total
+      row :status
+      row :created_at
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :status, as: :select, collection: Order.statuses.keys
+    end
+    f.actions
+  end
+
 end
